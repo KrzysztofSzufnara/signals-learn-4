22b03c6c09519054e86177ca794560e8ac1038cf
KrzysztofSzufnara <46495955+KrzysztofSzufnara@users.noreply.github.com>
2024-02-18 13:11:56 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SignalCounterComponent } from './sinal-counter/sinal-counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SignalCounterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'signals-learn-4';
+  parentCounter = 0;
 }
---
+++ src/app/sinal-counter/sinal-counter.component.ts
@@ -0,0 +1,16 @@
+import { Component, model } from '@angular/core';
+
+@Component({
+  selector: 'app-signal-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './sinal-counter.component.html',
+  styleUrl: './sinal-counter.component.css',
+})
+export class SignalCounterComponent {
+  counter = model(0);
+
+  increment() {
+    this.counter.update((c) => c + 1); // this.counter++;
+  }
+}
